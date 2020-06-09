e6e5f2420e83746ae0341496a448a4bc94723a49
gomster <gomster@ubuntuVM.com>
2020-06-09 14:14:18 -0400

    first commit

---
+++ sync.py
@@ -0,0 +1,6 @@
+import os
+os.system("git init")
+os.system("git add .")
+os.system("git status")
+os.system('git commit -m "first commit" ')
+os.system('git remote add origin https://github.com/gomsterX/competitive_programming.git')
