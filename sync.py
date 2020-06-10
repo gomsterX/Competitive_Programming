d0438e5b65193b065b7764b3d25535e6c0531437
gomster <gomster@ubuntuVM.com>
2020-06-10 04:26:47 -0400

    2020-06-10

--- sync.py
+++ sync.py
@@ -1,6 +1,8 @@
 import os
+from datetime import date
+
 os.system("git init")
 os.system("git add .")
 os.system("git status")
-os.system('git commit -m "first commit" ')
+os.system("git commit -m '{}' ".format(date.today()))
 os.system('git push -u origin')
