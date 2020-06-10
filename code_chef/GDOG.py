ea1aa1dc6e40fb49fcd668ae238b09cfb79bbcd8
gomster <gomster@ubuntuVM.com>
2020-06-10 06:45:54 -0400

    2020-06-10

--- code_chef/GDOG.py
+++ code_chef/GDOG.py
@@ -1,2 +1,14 @@
-#Problem ID: GDOG 
+#Problem ID: GDOG
 #Problem Name: Greedy puppy
+
+for _ in range(int(input())):
+    coins, no_of_people = map(int, input().split())
+
+    tuzik_coins = 0
+    if (coins < no_of_people):
+        tuzik_coins = coins
+    for i in range(1, no_of_people + 1):
+        if(coins % i > tuzik_coins):
+            tuzik_coins = coins%i
+
+    print(tuzik_coins)
